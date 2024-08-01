4d60b38fc9423896935d24dc4d12a74ab343ae69
ViniciusGavaPereira <vini-gava@outlook.com.br>
2024-07-31 21:59:51 -0300

    First commmit

---
+++ src/main/java/com/gateway/hotel_gateway/HotelGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.gateway.hotel_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HotelGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gateway/hotel_gateway/HotelGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.hotel_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
